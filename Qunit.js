660570be0e5ba9fca204e74056bfbb81569a420d
stefeanos <84009599+stefeanos@users.noreply.github.com>
2021-06-07 15:36:43 +0200

    opgave

--- Qunit.js
+++ Qunit.js
@@ -38,4 +38,51 @@ type="submit">button</button>`;
 
 
 
+elements.forEach(function (element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.innerHTML = color;
+  selectColor.innerHTML = color;
+});
 
+
+
+function quiz() {
+  let submitBtn;
+  submitBtn = submit();
+  quizPlay();
+  checkbox.disabled = true;
+  return submitBtn;
+
+}
+
+
+function getRandomColor() {
+
+  const letter = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letter[Math.floor(Math.random(1) * 16)];
+  }
+  return color;
+}
+
+
+document.querySelector("button[type=submit]");
+elements.forEach(function (element) {
+  element.addEventListener("click", function () {
+    if (element.innerHTML === selectColor.innerHTML) {
+      checkbox.checked = true;
+      alert("Du er menneske!");
+      submitBtn.disabled = false;
+      submitBtn.classList.remove("btn-light");
+
+      submitBtn.classList.add("btn-success");
+      console.log(submitBtn);
+    } else {
+      alert("vennligst bekreft at du er menneske!");
+      location.reload(true);
+    }
+
+  });
+});
