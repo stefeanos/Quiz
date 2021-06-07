701b18f77f8dd0ca4fcfa85bf044b715382d83df
stefeanos <84009599+stefeanos@users.noreply.github.com>
2021-06-07 10:19:29 +0200

    opgave

---
+++ Qunit.js
@@ -0,0 +1,41 @@
+
+function successHtml() {
+  return `
+<div class="form_container">
+<form action="success.html">
+`;
+}
+
+function checkboxHtml() {
+  return `
+<input type="checkbox" id="checkbox" />
+<label for="checkbox">checked</label>`;
+}
+
+
+function elementHtml() {
+  return `
+    <main class="container">
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    <div class="element"></div>
+    </main>`;
+}
+
+
+
+function submitHtml() {
+  return `
+<button class="btn btn-light"
+type="submit">button</button>`;
+}
+
+
+
+
